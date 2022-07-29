0e1a1fd907587fa99dbede13baa73227f62903d9
Joe AB <joe.anthonybrown@gmail.com>
2022-07-29 10:59:41 -0500

    got Java to run in VS code

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+    public static void main(String[] args) {
+      System.out.println("Hello Joe");
+      System.out.print("Oh hello...");
+      System.out.println("What are you doing here?");
+      System.out.print("Not much...");
+      System.out.println("Just learnin about Java...");
+    }
+  }
