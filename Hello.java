83af53331bfced4de49beeadbb092d32c5300fc5
Joe AB <joe.anthonybrown@gmail.com>
2022-07-30 13:22:05 -0500

    learning datatypes and variables in java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,20 @@
 
 public class Hello {
     public static void main(String[] args) {
+
+      // This stuff is nott too bad so far
+      // I am excited to learn!
+
       System.out.println("Hello Joe");
       System.out.print("Oh hello...");
       System.out.println("What are you doing here?");
       System.out.print("Not much...");
       System.out.println("Just learnin about Java...");
+
+      double androidShare = 81.7;
+      System.out.println(androidShare);
+
+      int numFingers = 2;
+      System.out.println(numFingers);
     }
   }
